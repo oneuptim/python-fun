f55c0ba41c6eb29c9773aeb07b98b434b3a17203
Tim Maina <oneuptim@Tims-MacBook-Pro.local>
2016-11-28 12:10:27 -0800

    Added some functions trials

--- hello.py
+++ hello.py
@@ -14,5 +14,22 @@ print 'How old are you?'
 age = input()
 
 # Return statements to give user feedback on what they have entered!
-print greeting, name, '!'
-print 'Being', age, 'must feel really old, huh?'
+if age > 30:
+    print greeting, name + '!', 'Being', age, 'must feel really old, huh?'
+elif age == 30:
+    print "Damn! You're getting old", name + '!'
+else:
+    print "Ahhh, you're just a youngin'", name + '!'
+
+# Add some space for readability
+print "------------------------------------------------------"
+print "------------------------------------------------------"
+
+# Trying out string interpolation!
+print "This is an interpolated greetint '{}' but hold up! {}, are you really {}?".format(greeting, name, age)
+
+# Prompt user for input whether they are really the age they asay they are
+response = raw_input()
+
+# Spit this back!
+print "You said {}".format(response), "and I gotta believe you cuz- women lie, men lie, numbers don't lie!"

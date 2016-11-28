72565e00841203c6b05fb37f765101e59dfc81a1
Tim Maina <oneuptim@Tims-MacBook-Pro.local>
2016-11-28 11:41:27 -0800

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+# Varibale to set our greeting message
+greeting = "What's up,"
+
+# Screen prompt to ask user for their name
+print 'What is your name?'
+
+# Input for user to enter their name
+name = raw_input()
+
+# Screen instruction to ask for user's age
+print 'How old are you?'
+
+# Prompt input to ask user for their age
+age = input()
+
+# Return statements to give user feedback on what they have entered!
+print greeting, name, '!'
+print 'Being', age, 'must feel really old, huh?'
